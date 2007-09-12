e9573e2dbe33fc5706173b14480b0ee8fa6ea700
nightwatch41 <nightwatch41@c21c009e-8f39-0410-915a-49ea6329ddb5>
2007-09-12 04:55:38 +0000

    make Celestial compilable, and fix lib1funcs

--- include/include/Celestial/AVTimeMarkerObservation.h
+++ include/include/Celestial/AVTimeMarkerObservation.h
@@ -4,9 +4,7 @@
  *     class-dump is Copyright (C) 1997-1998, 2000-2001, 2004-2006 by Steve Nygard.
  */
 
-#import "NSObject.h"
-
-@class NSArray;
+#import <Foundation/Foundation.h>
 
 @interface AVTimeMarkerObservation : NSObject
 {
