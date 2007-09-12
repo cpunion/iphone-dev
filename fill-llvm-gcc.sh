64f4b628a7df39363a0dfdccb30dfc1b2f347834
nightwatch41 <nightwatch41@c21c009e-8f39-0410-915a-49ea6329ddb5>
2007-09-12 02:01:43 +0000

    move for now

--- fill-llvm-gcc.sh
+++ fill-llvm-gcc.sh
@@ -1,64 +1,67 @@
 #!/bin/bash
-if getopts "l" the_option
-then
-    local_only=yes
-else
-    local_only=no
-fi
+local_only=no
+version=4.0
 
-if test -e llvm-gcc-4.2
+while getopts "lr:" the_option
+do
+    case the_option in
+        l)  local_only=yes  ;;
+        r)  version=$OPTARG ;;
+    esac
+done
+
+if test -e llvm-gcc-$version
 then
     if test $local_only = yes
     then
-        echo "I found the directory llvm-gcc-4.2."
+        echo "I found the directory llvm-gcc-$version."
     else
-        echo "I found the directory llvm-gcc-4.2, so I'm going to update it"
-        echo "to the newest revision of LLVM-GCC from LLVM's SVN repository."
-        svn update llvm-gcc-4.2 || exit 1
+        echo "I found the directory llvm-gcc-$version, so I'm going to update "
+        echo "it to the newest revision of LLVM-GCC from LLVM's SVN "
+        echo "repository."
+        svn update llvm-gcc-$version || exit 1
     fi
 else
     if test $local_only = yes
     then
-        echo "I didn't find the directory llvm-gcc-4.2/, and you've requested"
-        echo "not to check the Internet for updates."
+        echo "I didn't find the directory llvm-gcc-$version/, and you've "
+        echo "requested that I not check the Internet for updates."
         exit 1;
     fi
 
-    echo "I didn't find the llvm-gcc-4.2/ directory. I'm going to check out a"
-    echo "copy from LLVM's SVN server. If this doesn't work, check it out"
-    echo "manually to that directory and rerun this script."
-    svn co http://llvm.org/svn/llvm-project/llvm-gcc-4.2/trunk llvm-gcc-4.2
+    echo "I didn't find the llvm-gcc-$version/ directory. I'm going to check"
+    echo "out a copy from LLVM's SVN server. If this doesn't work, check it"
+    echo "out manually to that directory and rerun this script."
+    svn co http://llvm.org/svn/llvm-project/llvm-gcc-$version/trunk \
+        llvm-gcc-$version
 fi
 
 echo "Finding files in LLVM-GCC..."
-pushd llvm-gcc-4.2
-#find * -not -path "*.svn*" -and -not -name llvm-gcc-4.2 -and -not -path \
-#    "libstdc++-v3/*" -and -not -name libstdc++-v3 -print > \
-#    ../llvm-gcc-4.2-files.tmp
-find * -not -path "*.svn*" -not -name llvm-gcc-4.2 -print > \
-    ../llvm-gcc-4.2-files.tmp
+pushd llvm-gcc-$version
+find * -not -path "*.svn*" -not -name llvm-gcc-$version -print > \
+    ../llvm-gcc-$version-files.tmp
 popd
 
 echo "Copying files..."
-for f in `cat llvm-gcc-4.2-files.tmp`
+while read f
 do
     if test -e "llvm-gcc-iphone/$f"
     then
         echo "Skipping $f"
     else
-        if test -d "llvm-gcc-4.2/$f"
+        if test -d "llvm-gcc-$version/$f"
         then
             echo "Making directory llvm-gcc-iphone/$f"
             mkdir -p "llvm-gcc-iphone/$f" || exit 1
         else
-            echo "Copying llvm-gcc-4.2/$f to llvm-gcc-iphone/$f"
-            cp "llvm-gcc-4.2/$f" "llvm-gcc-iphone/$f" || exit 1
+            echo "Copying llvm-gcc-$version/$f to llvm-gcc-iphone/$f"
+            cp "llvm-gcc-$version/$f" "llvm-gcc-iphone/$f" || exit 1
         fi
     fi
-done
+done < llvm-gcc-$version-files.tmp
 
 echo "Removing temporary files..."
-rm -f llvm-gcc-4.2-files.tmp
+rm -f llvm-gcc-$version-files.tmp
 
 echo "All done!"
 
