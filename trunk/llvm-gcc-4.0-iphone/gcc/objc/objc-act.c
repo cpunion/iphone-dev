fbf18f9eb7cab33b9ad45dc70233b5d3fa618fc7
nightwatch41 <nightwatch41@c21c009e-8f39-0410-915a-49ea6329ddb5>
2007-09-12 18:54:58 +0000

    fix issue 8

--- trunk/llvm-gcc-4.0-iphone/gcc/objc/objc-act.c
+++ trunk/llvm-gcc-4.0-iphone/gcc/objc/objc-act.c
@@ -8273,9 +8273,13 @@ objc_build_synchronized (location_t start_locus, tree mutex, tree body)
 /* snarfed from /usr/include/ppc/setjmp.h */
 /* APPLE LOCAL radar 4404766 */
 #define JBLEN ((TARGET_64BIT) ? (26*2 + 18*2 + 129 + 1) : (26 + 18*2 + 129 + 1))
-#else
+#elif TARGET_ARM
+#define JBLEN 27
+#elif TARGET_I386
 /* snarfed from /usr/include/i386/{setjmp,signal}.h */
 #define JBLEN ((TARGET_64BIT) ? ((9 * 2) + 3 + 16) : (18))
+#else
+#error Add the correct JBLEN size for your target here!
 #endif
 /* APPLE LOCAL end mainline 2006-05-24 */
 
