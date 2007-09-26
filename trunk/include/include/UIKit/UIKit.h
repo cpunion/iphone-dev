fc3975299bb31b15b672d45d8ea4b35bbddb5851
allen.porter <allen.porter@c21c009e-8f39-0410-915a-49ea6329ddb5>
2007-09-26 07:27:05 +0000

    Add TelephonyUI.framework headers.

--- trunk/include/include/UIKit/UIKit.h
+++ trunk/include/include/UIKit/UIKit.h
@@ -27,6 +27,7 @@
 #import <UIKit/UITableColumn.h>
 #import <UIKit/UITextLabel.h>
 #import <UIKit/UITextView.h>
+#import <UIKit/UIThreePartButton.h>
 #import <UIKit/UITransitionView.h>
 #import <UIKit/UITransformAnimation.h>
 #import <UIKit/UIView.h>
